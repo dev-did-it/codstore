60fd510faff549dd4155240ec06a193e536e6f8d
Devante Wilson <14277918+devo-codes@users.noreply.github.com>
2022-07-07 21:18:59 -0400

    Added functionality for all games (mw, cw, vg), comparison with existing file contents, and logging

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# codstore
+# COD Store
+
+This repo is for web scraping Call of Duty store bundles and storing relevant information about them.
+
+It employs concurrency for better performance.
--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 # this python module searches for cod store bundles and captures info about them into a .csv
+import logging
+import os
+import concurrent.futures
 import requests as rq
 import pandas as pd
 from bs4 import BeautifulSoup
 from tqdm import tqdm
-import concurrent.futures
 
 
 # write dataframe to .csv
-def write_data(titles, urls, skus):
+def write_data(titles, urls, skus, game):
     try:
         # define dict for bundle data
-        bundle_dict = {}
-
-        # create columns for dataframe
-        bundle_dict['title'] = titles
-        bundle_dict['url'] = urls
-        bundle_dict['sku'] = skus
+        bundle_dict = {'title': titles, 'url': urls, 'sku': skus}
 
         # write dataframe to .csv
         bundle_df = pd.DataFrame.from_dict(data=bundle_dict)
-        bundle_df.to_csv(path_or_buf='C:/repos/codstore/data/bundles_vg.csv', mode='a')
+
+        # set file name and path
+        file_name = 'C:/repos/codstore/data/bundles_{game}.csv'.format(game=game)
+
+        # if file does not exist write header
+        if not os.path.exists(file_name):
+            bundle_df.to_csv(file_name, index=False)
+        else:  # else it exists so append without writing the header
+            bundle_df.to_csv(path_or_buf=file_name, mode='a', index=False, header=False)
     except Exception as ex:
-        print('Exception occurred in write_data(): %s' % ex)
+        logging.exception('Exception occurred in write_data(): %s' % ex)
 
 
 # perform an HTTP GET request to cod store with sku code
-def make_request(sku):
+def make_request(sku, game):
     try:
         # define variables
         title_list = []
@@ -33,9 +38,7 @@ def make_request(sku):
         sku_list = []
 
         # set url
-        url = 'https://my.callofduty.com/store/sku/{sku}/title/vg'.format(sku=sku)
-        # sample bundle that does not exist
-        # url = 'https://my.callofduty.com/store/sku/33954689/title/vg'
+        url = 'https://my.callofduty.com/store/sku/{sku}/title/{game}'.format(sku=sku, game=game)
 
         # set headers
         headers = {
@@ -43,7 +46,7 @@ def make_request(sku):
         }
 
         # perform HTTP GET request
-        with rq.get(url=url, headers=headers) as req:
+        with rq.get(url=url, headers=headers, timeout=1, allow_redirects=False) as req:
             req_content = req.content
 
         # parse html response content
@@ -52,44 +55,70 @@ def make_request(sku):
         bundle_url = soup.find(name='meta', attrs={'property': 'og:url'}).get('content')
 
         # determine if bundle exists
-        # if bundle_title != 'My Call of Duty® Bundles':
-        if 'Vintage Vanguard' in bundle_title:
+        if bundle_title != 'My Call of Duty® Bundles':
+        # if 'Vintage Vanguard' in bundle_title:
             bundle_title = bundle_title.replace(' | My Call of Duty® Bundles', '')
 
             title_list.append(bundle_title)
             url_list.append(bundle_url)
             sku_list.append(sku)
 
-            write_data(title_list, url_list, sku_list)
+            # invoke function to write data to file
+            write_data(title_list, url_list, sku_list, game)
 
             return bundle_title
         else:
             return None
     except Exception as ex:
-        print('Exception occurred in get_request(): %s' % ex)
+        logging.exception('Exception occurred in make_request(): %s' % ex)
 
 
 # main running function
 def main():
     try:
         # define variables
-        start = 33954500
-        stop = 33954700
-        skus = range(start, stop)
+        logging.basicConfig(filename='codstore.log', encoding='utf-8', level=logging.DEBUG, filemode='w')
+
+        # mw bundles
+        # start = 400003
+        # stop = 400512
+        # game = 'mw'
+
+        # cw bundles
+        # start = 29490000
+        # stop = 29492000
+        # game = 'cw'
+
+        # vg bundles
+        start = 33954444
+        stop = 33954664
+        game = 'vg'
+
+        initial_skus = range(start, stop)
+
+        # read skus from file for comparison with sku list
+        file_name = 'C:/repos/codstore/data/bundles_{game}.csv'.format(game=game)
+
+        if os.path.exists(file_name):
+            file_skus = pd.read_csv(filepath_or_buffer=file_name, usecols=['sku'])['sku'].to_list()
+            # exclude skus already present in file
+            skus = [s for s in initial_skus if s not in file_skus]
+        else:
+            skus = initial_skus
+
         skus_len = len(skus)
-        sku = 0
 
         # iterate through SKUs concurrently
         with tqdm(total=skus_len) as pbar:
             with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-                futures = {executor.submit(make_request, sku): sku for sku in skus}
+                futures = {executor.submit(make_request, sku, game): sku for sku in skus}
                 results = {}
                 for future in concurrent.futures.as_completed(futures):
                     soup = futures[future]
                     results[soup] = future.result()
                     pbar.update(1)
     except Exception as ex:
-        print('Exception occurred in make_request() for sku %i: %s' % (sku, ex))
+        logging.exception('Exception occurred in main(): %s' % ex)
 
 
 if __name__ == '__main__':
